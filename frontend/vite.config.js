4b8d9f9a22140df0af21da98241a450d06efc168
meshv <meshv1444@gmail.com>
2025-12-31 22:39:12 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_API_BASE_URL || "duka",
   server: {
     port: 3000,   // 👈 your new port
   },
